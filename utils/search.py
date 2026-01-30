f0d056be5902e5192f9f69b0982af1b3acf3826e
Gaurav Salvi <105053156+Garrixxc@users.noreply.github.com>
2026-01-30 09:44:03 -0800

    RPC error

--- utils/search.py
+++ utils/search.py
@@ -5,7 +5,8 @@ from utils.embeddings import embed_text
 SUPABASE_URL = os.environ["SUPABASE_URL"]
 SUPABASE_SERVICE_ROLE_KEY = os.environ["SUPABASE_SERVICE_ROLE_KEY"]
 SUPABASE_TABLE = os.environ["SUPABASE_TABLE"]
-SUPABASE_MATCH_RPC = os.environ["SUPABASE_MATCH_RPC"]
+SUPABASE_MATCH_RPC = os.getenv("SUPABASE_MATCH_RPC", "match_chunks")
+
 
 supabase = create_client(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY)
 
